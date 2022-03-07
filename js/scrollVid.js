c88686524bbf6fd45023734fa7d41514f087cec4
amdavis48 <amdavis48@tntech.edu>
2022-03-06 20:17:57 -0600

    website update

--- js/scrollVid.js
+++ js/scrollVid.js
@@ -2,16 +2,18 @@
 
     $(document).scroll(function() {
 
-        myID = document.getElementById("information");
+        showInfo = document.getElementById("information");
 
         var myScrollFunc = function () {
             var y = window.scrollY;
             if (y >= 350) {
-                myID.className = "information show"
+                showInfo.className = "information show"
             } else {
-                myID.className = "information hide"
+                showInfo.className = "information hide"
             }
         };
 
         window.addEventListener("scroll", myScrollFunc);
     });
+
+    
